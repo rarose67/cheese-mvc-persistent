ef195cfa303fe319a4f2a2956f11384d0f0681b5
rarose67 <robertrosestl67@gmail.com>
2018-06-17 17:39:13 -0500

    Completed part 2, add Category to Cheese forms.

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,15 @@
 package org.launchcode.models;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -16,6 +21,10 @@ public class Category {
     @NotNull
     @Size(min=3, max=15)
     private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     public Category() {
     }
